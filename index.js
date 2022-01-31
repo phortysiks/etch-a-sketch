0beacd9af9ecdc707674da741a02d1e4aebaadda
Charlie Cox <phortysiks@me.com>
2022-01-31 22:52:42 +0000

    Styling and logic completed

--- index.js
+++ index.js
@@ -16,7 +16,29 @@ createGrid(64);
 const gridItems = document.querySelectorAll('.grid-item');
 gridItems.forEach((item) => {
   item.addEventListener('mouseenter', () => {
-    item.style.background = 'red';
+    item.style.background = 'rgb(13, 53, 54)';
   });
 })
 
+const reload = document.getElementById('reload');
+
+function reloadGrid() {
+  gridItems.forEach((item) => {
+    item.style.background = 'none';
+  });
+}
+
+reload.onclick = function() {
+  reloadGrid()
+};
+
+let slider = document.querySelector('.slider');
+let sizeLabel = document.querySelector('.size-label');
+sizeLabel.textContent = `${slider.value}x${slider.value}`;
+slider.oninput = function() {
+  sizeLabel.textContent = `${this.value}x${this.value}`;
+};
+slider.onmouseup = function() {
+  reloadGrid();
+  createGrid(this.value);
+};
